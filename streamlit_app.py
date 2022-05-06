f40bc8c66d29f156fceaf58c6323ec30f7724408
starformonika <105049999+starformonika@users.noreply.github.com>
2022-05-06 14:10:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,6 @@ import streamlit
 
 streamlit.title('My Mum\'s new healthy dinner')
 streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & blueberry oatmeal')
+streamlit.text('Kale, spinach & rocket smoothie')
+streamlit.text('Hard-boiled free-range egg')
